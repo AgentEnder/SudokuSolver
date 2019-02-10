6a2297598c556147630e7669584fc5fa08637166
Craigory V Coppola <agentendercake@gmail.com>
2019-02-10 18:16:25 -0500

    Puzzle Display Function

--- CraigoryCoppola_Project1/Sudoku.cpp
+++ CraigoryCoppola_Project1/Sudoku.cpp
@@ -1,11 +1,10 @@
+#include<iostream>
 #include "Sudoku.h"
-
 
 
 Sudoku::Sudoku()
 {
 }
-
 
 Sudoku::~Sudoku()
 {
@@ -18,7 +17,7 @@ Sudoku::Sudoku(int ** puzzle_mtx)
 	{
 		for (int j = 0; j < 9; j++)
 		{
-			puzzle.data[i*9+j] =  puzzle_mtx[i][j]
+			puzzle.data[i * 9 + j] = puzzle_mtx[i][j];
 		}
 	}
 }
@@ -30,4 +29,37 @@ Sudoku::Sudoku(int * puzzle_arr)
 	{
 		puzzle.data[i] = (short)puzzle_arr[i];
 	}
+}
+
+void Sudoku::Display()
+{
+	for (int y = 0; y < 9; y++)
+	{
+		std::cout << "\t";
+		if (y!= 0 && y % 3 == 0)
+		{
+			std::cout << "---------------------" << std::endl << "\t";
+		}
+		for (int x = 0; x < 9; x++)
+		{
+			if (x!= 0 && x%3 == 0)
+			{
+				std::cout << "| ";
+			}
+			std::cout << puzzle.data[y * 9 + x] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+bool Sudoku::operator=(Sudoku other)
+{
+	for (int i = 0; i < 81; i++)
+	{
+		if (puzzle.data[i]!=other.puzzle.data[i])
+		{
+			return false;
+		}
+	}
+	return true;
 }
--- CraigoryCoppola_Project1/Sudoku.h
+++ CraigoryCoppola_Project1/Sudoku.h
@@ -14,5 +14,9 @@ public:
 
 	Sudoku(int ** puzzle_mtx); //Constructor given a 2*2 matrix
 	Sudoku(int * puzzle_arr); //Constructor given a 1*81 matrix
+
+	void Display();
+
+	bool operator=(Sudoku other);
 };
 
